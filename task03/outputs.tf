f91829cea18f8eea4d26d84ebbbf989f4b4eeef1
ginjupalliVenky <ginjupalli93@gmail.com>
2025-04-01 20:02:30 +0530

    Fix outputs in outputs.tf with descriptions

--- task03/outputs.tf
+++ task03/outputs.tf
@@ -1,11 +1,14 @@
 output "rg_id" {
-  value = data.azurerm_resource_group.existing.id
+  description = "The ID of the existing Resource Group"
+  value       = data.azurerm_resource_group.existing.id
 }
 
 output "sa_blob_endpoint" {
-  value = azurerm_storage_account.sa.primary_blob_endpoint
+  description = "The blob service primary endpoint of the storage account"
+  value       = azurerm_storage_account.sa.primary_blob_endpoint
 }
 
 output "vnet_id" {
-  value = azurerm_virtual_network.vnet.id
+  description = "The ID of the created Virtual Network"
+  value       = azurerm_virtual_network.vnet.id
 }
