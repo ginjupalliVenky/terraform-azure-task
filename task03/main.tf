ac52acb8ecf8a5bb61694daafea72e2916bb6710
ginjupalliVenky <ginjupalli93@gmail.com>
2025-04-01 20:24:57 +0530

    Fix Terraform configuration files

--- task03/main.tf
+++ task03/main.tf
@@ -2,15 +2,9 @@ provider "azurerm" {
   features {}
 }
 
-# Define the existing Resource Group (using data)
+# Import the existing Resource Group
 data "azurerm_resource_group" "existing" {
   name = var.rg_name
-}
-
-# (Optional) If you need to **create** a new resource group instead of importing, uncomment this block:
-resource "azurerm_resource_group" "rg" {
-  name     = var.rg_name
-  location = "East US"  # Update to your preferred location
 }
 
 # Storage Account Resource
@@ -18,13 +12,15 @@ resource "azurerm_storage_account" "sa" {
   name                     = var.storageaccount_name
   resource_group_name      = data.azurerm_resource_group.existing.name
   location                 = data.azurerm_resource_group.existing.location
-  account_tier              = "Standard"
-  account_replication_type = "LRS"
+  account_tier             = "Standard" # Use "Standard" instead of "Basic"
+  account_replication_type = "LRS"      # This is fine for standard accounts
 
   tags = {
     Creator = var.student_email
   }
 }
+
+
 
 # Virtual Network Resource
 resource "azurerm_virtual_network" "vnet" {
@@ -38,7 +34,8 @@ resource "azurerm_virtual_network" "vnet" {
   }
 }
 
-# First Subnet Resource
+
+# First Subnet Resource (Frontend)
 resource "azurerm_subnet" "subnet1" {
   name                 = var.subnet1_name
   resource_group_name  = data.azurerm_resource_group.existing.name
@@ -46,7 +43,7 @@ resource "azurerm_subnet" "subnet1" {
   address_prefixes     = ["10.0.1.0/24"]
 }
 
-# Second Subnet Resource
+# Second Subnet Resource (Backend)
 resource "azurerm_subnet" "subnet2" {
   name                 = var.subnet2_name
   resource_group_name  = data.azurerm_resource_group.existing.name
--- task03/outputs.tf
+++ task03/outputs.tf
@@ -1,14 +1,14 @@
 output "rg_id" {
-  description = "The ID of the existing Resource Group"
+  description = "The ID of the Resource Group"
   value       = data.azurerm_resource_group.existing.id
 }
 
 output "sa_blob_endpoint" {
-  description = "The blob service primary endpoint of the storage account"
+  description = "The blob service primary endpoint of the Storage Account"
   value       = azurerm_storage_account.sa.primary_blob_endpoint
 }
 
 output "vnet_id" {
-  description = "The ID of the created Virtual Network"
+  description = "The ID of the Virtual Network"
   value       = azurerm_virtual_network.vnet.id
 }
